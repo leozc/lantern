0804e97c3f66431f2c5a86e3dcd7907810e64a5f
Leo Liang <leozc@Leozc.com>
2013-01-17 00:33:21 -0800

    Initial check in with new repo name

---
+++ src/main/java/com/leozc/lantern/Blacklister.java
@@ -0,0 +1,102 @@
+package com.leozc.lantern;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.sun.org.apache.bcel.internal.classfile.StackMapEntry;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Mojo(name="blacklist")
+public class Blacklister extends AbstractMojo {
+
+    @Parameter(required = true,property = "latern.rulefile")
+    private String rulefile = null;
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        File f = new File("target/dependency");
+        File rule = new File(rulefile);
+        List<RuleEntry> rules = new ArrayList<RuleEntry>();
+
+        System.out.println("Rule => "+ rulefile);
+//        for(Object e: this.getPluginContext().keySet()){
+//            System.out.println(e + " => "+ this.getPluginContext().get(e));
+//        }
+//
+        if(rule.exists()){
+            try {
+                Type listType = new TypeToken<ArrayList<RuleEntry>>() {
+                }.getType();
+                rules  = new Gson().fromJson(FileUtils.fileRead(rule),listType);
+
+            } catch (IOException e) {
+                throw new MojoFailureException("cannot read/parse "+rulefile + " ERROR "+e.getMessage());
+            }
+        }
+
+
+        if(f.exists()){
+            File[] scantargetJars =  f.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".jar");
+                }
+            });
+
+
+            System.out.println("====== Found the dependency folder " + scantargetJars.length + " scantargetJars found.");
+            System.out.println(scantargetJars[0].getAbsolutePath());
+            String outputfilename = "lantern.blacklist.out";
+            try{
+                FileWriter fw = new FileWriter(outputfilename,false);
+
+                for(RuleEntry r : rules){
+                    for(File jarpath:scantargetJars)
+                        try {
+                            MethodcallerMatcher app = new MethodcallerMatcher();
+                            app.findCallingMethodsInJar(jarpath.getAbsolutePath(),r.className,r.methodDescriptor);
+                            for (MethodcallerMatcher.Callee c : app.getCallees()) {
+                                String outStr = jarpath.getName()+":"+c.source+":L"+c.line+":"+c.className+"."+c.methodName+"@"+c.methodDesc + ":calls "+r.className+"."+r.methodDescriptor;
+
+
+                                System.out.println("@@:"+ c.source+":L"+c.line+":"+":calls "+r.className);
+                                fw.write(outStr+"\n");
+                            }
+                            System.out.println("@@\n"+app.getCallees().size()+" methods invoke "+
+                                    app.getTargetClass()+" "+
+                                    app.getTargetMethod().getName()+" "+app.getTargetMethod().getDescriptor());
+
+                        } catch (Exception e) {
+                            System.err.println();
+                        }
+                }
+                System.out.println(outputfilename +" is generated.");
+            }   catch (IOException ioe){
+                throw new MojoFailureException("Fail to write output log");
+            }
+
+        }
+        else{
+            System.out.println("======= NO");
+            throw new MojoFailureException("please run 'mvn dependency:copy-dependencies' to pull down the dependencies to target/dependency folder before running this plugin");
+        }
+    }
+
+
+
+    protected class RuleEntry{
+        String className;
+        String methodDescriptor;
+    }
+}
---
+++ src/main/java/com/leozc/lantern/MethodcallerMatcher.java
@@ -0,0 +1,175 @@
+package com.leozc.lantern;
+import com.sun.xml.internal.ws.org.objectweb.asm.Label;
+import org.apache.commons.lang3.StringUtils;
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.MethodAdapter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.commons.*;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class MethodcallerMatcher {
+    private String targetClass;
+    private Method targetMethod;
+
+    private AppClassVisitor cv;
+
+    private ArrayList<Callee> callees = new ArrayList<Callee>();
+    public List<Callee> getCallees(){
+        return callees;
+    }
+
+    public String getTargetClass() {
+        return targetClass;
+    }
+
+    public Method getTargetMethod() {
+        return targetMethod;
+    }
+
+    protected  class Callee {
+        String className;
+        String methodName;
+        String methodDesc;
+        String source;
+        int line;
+
+        public Callee(String cName, String mName, String mDesc, String src, int ln) {
+            className = cName; methodName = mName; methodDesc = mDesc; source = src; line = ln;
+        }
+    }
+
+    private class AppMethodVisitor extends MethodAdapter {
+
+        boolean callsTarget;
+        int line;
+
+        public AppMethodVisitor() { super(new EmptyVisitor()); }
+
+        public void visitMethodInsn(int opcode, String owner, String name, String desc) {
+            if (owner.equals(targetClass)
+                    && name.equals(targetMethod.getName())
+                    && desc.equals(targetMethod.getDescriptor())
+                    )
+            {
+                callsTarget = true;
+            }
+        }
+
+        public void visitCode() {
+            callsTarget = false;
+        }
+
+        public void visitLineNumber(int line, Label start) {
+            this.line = line;
+        }
+
+        public void visitEnd() {
+            if (callsTarget)
+                callees.add(new Callee(cv.className, cv.methodName, cv.methodDesc,
+                        cv.source, line));
+        }
+    }
+
+    private class AppClassVisitor extends ClassAdapter {
+
+        private AppMethodVisitor mv = new AppMethodVisitor();
+
+        public String source;
+        public String className;
+        public String methodName;
+        public String methodDesc;
+
+        public AppClassVisitor() { super(new EmptyVisitor()); }
+
+        public void visit(int version, int access, String name,
+                          String signature, String superName, String[] interfaces) {
+            className = name;
+        }
+
+        public void visitSource(String source, String debug) {
+            this.source = source;
+        }
+
+        public MethodVisitor visitMethod(int access, String name,
+                                         String desc, String signature,
+                                         String[] exceptions) {
+            methodName = name;
+            methodDesc = desc;
+
+            return mv;
+        }
+    }
+
+
+    public void findCallingMethodsInJar(String jarPath, String targetClass,
+                                        String targetMethodDeclaration) throws Exception {
+
+        this.targetClass = targetClass;
+        this.targetMethod = Method.getMethod(targetMethodDeclaration);
+
+        this.cv = new AppClassVisitor();
+
+        JarFile jarFile = new JarFile(jarPath);
+        Enumeration<JarEntry> entries = jarFile.entries();
+
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+
+            if (entry.getName().endsWith(".class")) {
+                InputStream stream = new BufferedInputStream(jarFile.getInputStream(entry), 1024);
+                ClassReader reader = new ClassReader(stream);
+
+                reader.accept(cv, 0);
+
+                stream.close();
+            }
+        }
+    }
+
+
+    public static void main( String[] args ) {
+
+        if(args.length<3){
+            System.err.println("It takes three parameters $LIB $CLASS $MethodDesc");
+            System.err.println("See run.sh for example");
+            System.exit(-1);
+
+        }
+        try {
+            MethodcallerMatcher app = new MethodcallerMatcher();
+
+            String classname = args[0];
+            String methoddesc = args[1];
+
+            System.out.println("CLASS: "+classname);
+            System.out.println("MethodDesc: "+methoddesc);
+            System.out.println("LIBs: "+ StringUtils.join(Arrays.asList(args).subList(2,args.length-1), ";"));
+
+            for(int i=2;i<args.length;i++){   //for each libs
+                app.findCallingMethodsInJar(args[i], classname, methoddesc);
+
+                for (Callee c : app.callees) {
+                    System.out.println("== "+c.source+":"+c.line+" "+c.className+" "+c.methodName+" "+c.methodDesc);
+                }
+
+                System.out.println("--\n"+app.callees.size()+" methods invoke "+
+                        app.targetClass+" "+
+                        app.targetMethod.getName()+" "+app.targetMethod.getDescriptor());
+            }
+
+
+        } catch(Exception x) {
+            x.printStackTrace();
+        }
+    }
+
+}
