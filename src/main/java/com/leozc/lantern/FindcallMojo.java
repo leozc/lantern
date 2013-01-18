e2fb94711777b4dbead600789079dbdd3d5a821e
Leo Liang <leol@zillow.com>
2013-01-18 15:08:49 -0800

    Make the plugin be able to scan subfolder

--- src/main/java/com/leozc/lantern/FindcallMojo.java
+++ src/main/java/com/leozc/lantern/FindcallMojo.java
@@ -2,12 +2,12 @@ package com.leozc.lantern;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.sun.org.apache.bcel.internal.classfile.StackMapEntry;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
@@ -16,25 +16,36 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
-@Mojo(name="blacklist")
-public class Blacklister extends AbstractMojo {
+@Mojo(name="findcaller")
+public class FindcallMojo extends AbstractMojo {
 
     @Parameter(required = true,property = "lantern.rulefile")
     private String rulefile = null;
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        File f = new File("target/dependency");
+
+        MavenProject p = (MavenProject)this.getPluginContext().get("project");
+        final String packagetype = p.getPackaging();
+        if(!packagetype.equals("jar") && !packagetype.equals("war"))
+        {
+            getLog().info("Project packaging type "+p.getPackaging() +" unsupported, skipping....");
+            return;
+        }
+
+
+
+
+        getLog().info("Rule => " + rulefile);
+
+        /////////////////
+        // PARSE THE RULES
+        //////////////////
         File rule = new File(rulefile);
         List<RuleEntry> rules = new ArrayList<RuleEntry>();
-
-        System.out.println("Rule => "+ rulefile);
-//        for(Object e: this.getPluginContext().keySet()){
-//            System.out.println(e + " => "+ this.getPluginContext().get(e));
-//        }
-//
         if(rule.exists()){
             try {
                 Type listType = new TypeToken<ArrayList<RuleEntry>>() {
@@ -46,45 +57,59 @@ public class Blacklister extends AbstractMojo {
             }
         }
 
-
-        if(f.exists()){
-            File[] scantargetJars =  f.listFiles(new FilenameFilter() {
+        File dependancyPath = new File(p.getBuild().getDirectory()+"/dependency");
+        File buildPath      = new File(p.getBuild().getDirectory());
+        if(buildPath.exists() && dependancyPath.exists()){
+            File[] scantargetPackages =  dependancyPath.listFiles(new FilenameFilter() {
                 public boolean accept(File dir, String name) {
                     return name.toLowerCase().endsWith(".jar");
                 }
             });
-            if(scantargetJars.length==0){
+            File[] scantargetArtifact =  buildPath.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(packagetype);
+                }
+            });
+
+            List<File> files = new ArrayList<File>();
+
+            files.addAll(Arrays.asList(scantargetPackages));
+            files.addAll(Arrays.asList(scantargetArtifact));
+
+
+
+            if(scantargetPackages.length==0){
                 throw new MojoFailureException("No jar found ? please run 'mvn dependency:copy-dependencies' to pull down the dependencies to target/dependency folder before running this plugin");
             }
-            System.out.println("====== Found the dependency folder " + scantargetJars.length + " scantargetJars found.");
-            System.out.println(scantargetJars[0].getAbsolutePath());
+            getLog().info("====== Found the dependency folder " + scantargetPackages.length + " scantargetPackages found.");
+            getLog().info(scantargetPackages[0].getAbsolutePath());
             String outputfilename = "lantern.blacklist.out";
             FileWriter fw = null;
+
             try{
                 fw = new FileWriter(outputfilename,false);
-
                 for(RuleEntry r : rules){
-                    for(File jarpath:scantargetJars)
+                    for(File jarpath:files)
                         try {
                             MethodcallerMatcher app = new MethodcallerMatcher();
                             app.findCallingMethodsInJar(jarpath.getAbsolutePath(),r.className,r.methodDescriptor);
                             for (MethodcallerMatcher.Callee c : app.getCallees()) {
-                                String outStr = jarpath.getName()+":"+c.source+":L"+c.line+":"+c.className+"."+c.methodName+"@"+c.methodDesc + ":calls "+r.className+"."+r.methodDescriptor;
+                                String outStr = p.getArtifactId()+":"+jarpath.getName()+":"+c.source+":L"+c.line+":"+c.className+"."+c.methodName+"@"+c.methodDesc + ":calls "+r.className+"."+r.methodDescriptor;
 
-
-                                System.out.println("@@:"+ c.source+":L"+c.line+":"+":calls "+r.className);
+                                getLog().info("@@:" + c.source + ":L" + c.line + ":" + ":calls " + r.className);
                                 fw.write(outStr+"\n");
                             }
-                            System.out.println("@@\n"+app.getCallees().size()+" methods invoke "+
-                                    app.getTargetClass()+" "+
-                                    app.getTargetMethod().getName()+" "+app.getTargetMethod().getDescriptor());
+
+                            getLog().info("@@" + app.getCallees().size() + " methods invoke " +
+                                    app.getTargetClass() + " " +
+                                    app.getTargetMethod().getName() + " " + app.getTargetMethod().getDescriptor());
 
                         } catch (Exception e) {
-                            System.err.println();
+                            getLog().error("Fail to process"+jarpath.getName(),e);
                         }
                 }
 
-                System.out.println(outputfilename +" is generated.");
+                getLog().info(outputfilename + " is generated.");
             }   catch (IOException ioe){
                 throw new MojoFailureException("Fail to write output log");
             }   finally {
@@ -98,8 +123,9 @@ public class Blacklister extends AbstractMojo {
 
         }
         else{
-            System.out.println("======= NO");
-            throw new MojoFailureException("please run 'mvn dependency:copy-dependencies' to pull down the dependencies to target/dependency folder before running this plugin");
+            getLog().error("Cannnot find dependency folder");
+            getLog().error("please run 'mvn dependency:copy-dependencies' to pull down the dependencies to target/dependency folder before running this plugin");
+            throw new MojoFailureException(this,"fail to find target/dependency for "+ p.getArtifact().getArtifactId(),"run 'mvn dependency:copy-dependencies' ");
         }
     }
 
